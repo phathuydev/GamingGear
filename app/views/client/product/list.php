a21187fee28d5ed2457570e4b719eadbc3348c46
phuong05429 <phuongncpc05429@fpt.edu.vn>
2023-11-13 22:48:26 +0700

    GG-08-product

--- app/views/client/product/list.php
+++ app/views/client/product/list.php
@@ -1,210 +1,352 @@
 <div class="row">
-  <div class="col-lg-12 p-0">
-    <div class="page-content m-0">
+  <div class="col-lg-12">
 
+    <div class="page-content p-5">
+      <div class="heading-section text-center">
+        <h4><em>All</em> Products</h4>
+      </div>
       <!-- ***** Featured Games Start ***** -->
       <div class="row">
-        <div class="col-lg-8">
-          <div class="featured-games header-text mb-5">
-            <div class="heading-section">
-              <h4>All Product</h4>
+        <div class=" featured-games header-text mb-5">
+
+          <div class="card-pd">
+
+            <div class="imgBox">
+              <img src="<?php echo _WEB_ROOT; ?>/public/assets/client/images/product-1.png" alt="mouse corsair" class="mouse">
             </div>
-            <div class="owl-features owl-carousel">
-              <div class="item">
-                <div class="thumb">
-                  <img src="https://pc-laptop-center.com/images/product_images/original_images/GamerTastaturweiss.jpg" alt="">
-                  <div class="hover-effect">
-                    <h6>2400 View</h6>
-                  </div>
-                </div>
-                <div class="text-center">
-                  <label class="top-0 mt-5 position-absolute start-0 rounded-circle pt-2 pb-2 btn-danger">-20%</label>
-                  <h4>Fortnite</h4>
-                  <div class="d-flex justify-content-center align-items-center mt-2">
-                    <p class="text-danger me-2">600.000đ</p>
-                    <s class="text-muted">900.000đ</s>
-                  </div>
-                  <button class="btn-add-to-cart">Add To Cart</button>
-                </div>
-              </div>
-              <div class="item">
-                <div class="thumb">
-                  <img src="https://pc-laptop-center.com/images/product_images/original_images/GamerTastaturweiss.jpg" alt="" width="150">
-                  <div class="hover-effect">
-                    <h6>2400 View</h6>
-                  </div>
-                </div>
-                <div class="text-center">
-                  <label class="top-0 mt-5 position-absolute start-0 rounded-circle pt-2 pb-2 btn-danger">-20%</label>
-                  <h4>Fortnite</h4>
-                  <div class="d-flex justify-content-center align-items-center mt-2">
-                    <p class="text-danger me-2">600.000đ</p>
-                    <s class="text-muted">900.000đ</s>
-                  </div>
-                  <button class="btn-add-to-cart">Add To Cart</button>
-                </div>
-              </div>
-              <div class="item">
-                <div class="thumb">
-                  <img src="https://pc-laptop-center.com/images/product_images/original_images/GamerTastaturweiss.jpg" alt="" width="150">
-                  <div class="hover-effect">
-                    <h6>2400 View</h6>
-                  </div>
-                </div>
-                <div class="text-center">
-                  <label class="top-0 mt-5 position-absolute start-0 rounded-circle pt-2 pb-2 btn-danger">-20%</label>
-                  <h4>Fortnite</h4>
-                  <div class="d-flex justify-content-center align-items-center mt-2">
-                    <p class="text-danger me-2">600.000đ</p>
-                    <s class="text-muted">900.000đ</s>
-                  </div>
-                  <button class="btn-add-to-cart">Add To Cart</button>
-                </div>
-              </div>
-              <div class="item">
-                <div class="thumb">
-                  <img src="https://pc-laptop-center.com/images/product_images/original_images/GamerTastaturweiss.jpg" alt="" width="150">
-                  <div class="hover-effect">
-                    <h6>2400 View</h6>
-                  </div>
-                </div>
-                <div class="text-center">
-                  <label class="top-0 mt-5 position-absolute start-0 rounded-circle pt-2 pb-2 btn-danger">-20%</label>
-                  <h4>Fortnite</h4>
-                  <div class="d-flex justify-content-center align-items-center mt-2">
-                    <p class="text-danger me-2">600.000đ</p>
-                    <s class="text-muted">900.000đ</s>
-                  </div>
-                  <button class="btn-add-to-cart">Add To Cart</button>
-                </div>
-              </div>
-              <div class="item">
-                <div class="thumb">
-                  <img src="https://pc-laptop-center.com/images/product_images/original_images/GamerTastaturweiss.jpg" alt="" width="150">
-                  <div class="hover-effect">
-                    <h6>2400 View</h6>
-                  </div>
-                </div>
-                <div class="text-center">
-                  <label class="top-0 mt-5 position-absolute start-0 rounded-circle pt-2 pb-2 btn-danger">-20%</label>
-                  <h4>Fortnite</h4>
-                  <div class="d-flex justify-content-center align-items-center mt-2">
-                    <p class="text-danger me-2">600.000đ</p>
-                    <s class="text-muted">900.000đ</s>
-                  </div>
-                  <button class="btn-add-to-cart">Add To Cart</button>
-                </div>
-              </div>
-              <div class="item">
-                <div class="thumb">
-                  <img src="https://pc-laptop-center.com/images/product_images/original_images/GamerTastaturweiss.jpg" alt="" width="150">
-                  <div class="hover-effect">
-                    <h6>2400 View</h6>
-                  </div>
-                </div>
-                <div class="text-center">
-                  <label class="top-0 mt-5 position-absolute start-0 rounded-circle pt-2 pb-2 btn-danger">-20%</label>
-                  <h4>Fortnite</h4>
-                  <div class="d-flex justify-content-center align-items-center mt-2">
-                    <p class="text-danger me-2">600.000đ</p>
-                    <s class="text-muted">900.000đ</s>
-                  </div>
-                  <button class="btn-add-to-cart">Add To Cart</button>
-                </div>
-              </div>
+
+            <div class="contentBox">
+              <h3>Mouse Corsair M65</h3>
+              <h2 class="price">61.<small>98</small> $</h2>
+              <a href="#" class="buy">Add to cart</a>
             </div>
+
           </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="top-downloaded pb-0">
-            <div class="heading-section">
-              <h4>Top View</h4>
+
+          <div class="card-pd">
+
+            <div class="imgBox">
+              <img src="<?php echo _WEB_ROOT; ?>/public/assets/client/images/product-1.png" alt="mouse corsair" class="mouse">
             </div>
-            <div>
-              <ul>
-                <li>
-                  <img src="https://pc-laptop-center.com/images/product_images/original_images/GamerTastaturweiss.jpg" alt="" class="templatemo-item">
-                  <h4>Fortnite</h4>
-                  <h6>Sandbox</h6>
-                  <span><i class="fa fa-star" style="color: yellow;"></i> 4.9</span>
-                  <span><i class="fa fa-eye" style="color: #ec6090;"></i> 2.2M</span>
-                  <div class="download">
-                    <a href="#"><i class="fa fa-eye"></i></a>
-                  </div>
-                </li>
-                <li>
-                  <img src="https://pc-laptop-center.com/images/product_images/original_images/GamerTastaturweiss.jpg" alt="" class="templatemo-item">
-                  <h4>CS-GO</h4>
-                  <h6>Legendary</h6>
-                  <span><i class="fa fa-star" style="color: yellow;"></i> 4.9</span>
-                  <span><i class="fa fa-eye" style="color: #ec6090;"></i> 2.2M</span>
-                  <div class="download">
-                    <a href="#"><i class="fa fa-eye"></i></a>
-                  </div>
-                </li>
-                <li>
-                  <img src="https://pc-laptop-center.com/images/product_images/original_images/GamerTastaturweiss.jpg" alt="" class="templatemo-item">
-                  <h4>PugG</h4>
-                  <h6>Battle Royale</h6>
-                  <span><i class="fa fa-star" style="color: yellow;"></i> 4.9</span>
-                  <span><i class="fa fa-eye" style="color: #ec6090;"></i> 2.2M</span>
-                  <div class="download">
-                    <a href="#"><i class="fa fa-eye"></i></a>
-                  </div>
-                </li>
-              </ul>
+
+            <div class="contentBox">
+              <h3>Mouse Corsair M65</h3>
+              <h2 class="price">61.<small>98</small> $</h2>
+              <a href="#" class="buy">Add to cart</a>
             </div>
+
           </div>
+
+          <div class="card-pd">
+
+            <div class="imgBox">
+              <img src="<?php echo _WEB_ROOT; ?>/public/assets/client/images/product-1.png" alt="mouse corsair" class="mouse">
+            </div>
+
+            <div class="contentBox">
+              <h3>Mouse Corsair M65</h3>
+              <h2 class="price">61.<small>98</small> $</h2>
+              <a href="#" class="buy">Add to cart</a>
+            </div>
+
+          </div>
+
+
+
+
         </div>
-        <div class="col-lg-12">
-          <div class="featured-games header-text">
-            <div class="heading-section">
-              <h4>Product Category</h4>
+
+        <div class=" featured-games header-text mb-5">
+
+          <div class="card-pd">
+
+            <div class="imgBox">
+              <img src="<?php echo _WEB_ROOT; ?>/public/assets/client/images/product-1.png" alt="mouse corsair" class="mouse">
             </div>
-            <div class="owl-features owl-carousel mb-5">
-              <div class="item">
-                <div class="thumb text-center">
-                  <img src="https://pc-laptop-center.com/images/product_images/original_images/GamerTastaturweiss.jpg" alt="" width="150">
-                  <div class="down-content text-center mt-3">
-                    <h4 style="color: #ec6090;" class="m-0"></i> Mouse</h4>
-                  </div>
+
+            <div class="contentBox">
+              <h3>Mouse Corsair M65</h3>
+              <h2 class="price">61.<small>98</small> $</h2>
+              <a href="#" class="buy">Add to cart</a>
+            </div>
+
+          </div>
+
+          <div class="card-pd">
+
+            <div class="imgBox">
+              <img src="<?php echo _WEB_ROOT; ?>/public/assets/client/images/product-1.png" alt="mouse corsair" class="mouse">
+            </div>
+
+            <div class="contentBox">
+              <h3>Mouse Corsair M65</h3>
+              <h2 class="price">61.<small>98</small> $</h2>
+              <a href="#" class="buy">Add to cart</a>
+            </div>
+
+          </div>
+
+          <div class="card-pd">
+
+            <div class="imgBox">
+              <img src="<?php echo _WEB_ROOT; ?>/public/assets/client/images/product-1.png" alt="mouse corsair" class="mouse">
+            </div>
+
+            <div class="contentBox">
+              <h3>Mouse Corsair M65</h3>
+              <h2 class="price">61.<small>98</small> $</h2>
+              <a href="#" class="buy">Add to cart</a>
+            </div>
+
+          </div>
+
+
+
+
+        </div>
+
+        <div class=" featured-games header-text mb-5">
+
+          <div class="card-pd">
+
+            <div class="imgBox">
+              <img src="<?php echo _WEB_ROOT; ?>/public/assets/client/images/product-1.png" alt="mouse corsair" class="mouse">
+            </div>
+
+            <div class="contentBox">
+              <h3>Mouse Corsair M65</h3>
+              <h2 class="price">61.<small>98</small> $</h2>
+              <a href="#" class="buy">Add to cart</a>
+            </div>
+
+          </div>
+
+          <div class="card-pd">
+
+            <div class="imgBox">
+              <img src="<?php echo _WEB_ROOT; ?>/public/assets/client/images/product-1.png" alt="mouse corsair" class="mouse">
+            </div>
+
+            <div class="contentBox">
+              <h3>Mouse Corsair M65</h3>
+              <h2 class="price">61.<small>98</small> $</h2>
+              <a href="#" class="buy">Add to cart</a>
+            </div>
+
+          </div>
+
+          <div class="card-pd">
+
+            <div class="imgBox">
+              <img src="<?php echo _WEB_ROOT; ?>/public/assets/client/images/product-1.png" alt="mouse corsair" class="mouse">
+            </div>
+
+            <div class="contentBox">
+              <h3>Mouse Corsair M65</h3>
+              <h2 class="price">61.<small>98</small> $</h2>
+              <a href="#" class="buy">Add to cart</a>
+            </div>
+
+          </div>
+
+
+
+
+        </div>
+
+      </div>
+    </div>
+
+    <div class="most-popular">
+
+
+
+      <div class="col-lg-12">
+        <div class="heading-section text-center">
+          <h4><em>All</em> Category</h4>
+        </div>
+        <div class="featured-games header-text">
+
+          <div class="owl-features owl-carousel mb-5">
+
+            <div class="container">
+              <div class="card">
+                <div class="imgBx">
+                  <img src="<?php echo _WEB_ROOT; ?>/public/assets/client/images/product-category.png" alt="headphone">
                 </div>
-              </div>
-              <div class="item">
-                <div class="thumb">
-                  <img src="https://pc-laptop-center.com/images/product_images/original_images/GamerTastaturweiss.jpg" alt="" width="150">
-                  <div class="down-content text-center mt-3">
-                    <h4 style="color: #ec6090;" class="m-0"></i> Mouse</h4>
+
+                <div class="contentBx">
+
+                  <h2>Head Phone</h2>
+
+                  <div class="size">
+                    <h3>status :</h3>
+                    <span>active</span>
+
                   </div>
-                </div>
-              </div>
-              <div class="item">
-                <div class="thumb">
-                  <img src="https://pc-laptop-center.com/images/product_images/original_images/GamerTastaturweiss.jpg" alt="" width="150">
-                  <div class="down-content text-center mt-3">
-                    <h4 style="color: #ec6090;" class="m-0"></i> Mouse</h4>
+
+                  <div class="color">
+
+                    <h3>Color :</h3>
+                    <span></span>
+                    <span></span>
+                    <span></span>
                   </div>
+                  <a href="#">View Now</a>
                 </div>
-              </div>
-              <div class="item">
-                <div class="thumb">
-                  <img src="https://pc-laptop-center.com/images/product_images/original_images/GamerTastaturweiss.jpg" alt="" width="150">
-                  <div class="down-content text-center mt-3">
-                    <h4 style="color: #ec6090;" class="m-0"></i> Mouse</h4>
-                  </div>
-                </div>
-              </div>
-              <div class="item">
-                <div class="thumb">
-                  <img src="https://pc-laptop-center.com/images/product_images/original_images/GamerTastaturweiss.jpg" alt="" width="150">
-                  <div class="down-content text-center mt-3">
-                    <h4 style="color: #ec6090;" class="m-0"></i> Mouse</h4>
-                  </div>
-                </div>
+
               </div>
             </div>
+
+            <div class="container">
+              <div class="card">
+                <div class="imgBx">
+                  <img src="<?php echo _WEB_ROOT; ?>/public/assets/client/images/product-category-2.png" alt="keyboard">
+                </div>
+
+                <div class="contentBx">
+
+                  <h2>Keyboard</h2>
+
+                  <div class="size">
+                    <h3>status :</h3>
+                    <span>sold out</span>
+                  </div>
+
+                  <div class="color">
+
+                    <h3>Color :</h3>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </div>
+                  <a href="#">View Now</a>
+                </div>
+
+              </div>
+            </div>
+
+            <div class="container">
+              <div class="card">
+                <div class="imgBx">
+                  <img src="<?php echo _WEB_ROOT; ?>/public/assets/client/images/product-category-3.png" alt="mouse">
+                </div>
+
+                <div class="contentBx">
+
+                  <h2>Mouse</h2>
+
+                  <div class="size">
+                    <h3>status :</h3>
+                    <span>active</span>
+                  </div>
+
+                  <div class="color">
+
+                    <h3>Color :</h3>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </div>
+                  <a href="#">View Now</a>
+                </div>
+
+              </div>
+            </div>
+
+            <div class="container">
+              <div class="card">
+                <div class="imgBx">
+                  <img src="<?php echo _WEB_ROOT; ?>/public/assets/client/images/product-category-5.png" alt="card">
+                </div>
+
+                <div class="contentBx">
+
+                  <h2>Graphics Card</h2>
+
+                  <div class="size">
+                    <h3>status :</h3>
+                    <span>active</span>
+                  </div>
+
+                  <div class="color">
+
+                    <h3>Color :</h3>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </div>
+                  <a href="#">View Now</a>
+                </div>
+
+              </div>
+            </div>
+
+            <div class="container">
+              <div class="card">
+                <div class="imgBx">
+                  <img src="<?php echo _WEB_ROOT; ?>/public/assets/client/images/product-category-6.png" alt="nike-air-shoe">
+                </div>
+
+                <div class="contentBx">
+
+                  <h2>Game Pad</h2>
+
+                  <div class="size">
+                    <h3>status :</h3>
+                    <span>active</span>
+                  </div>
+
+                  <div class="color">
+
+                    <h3>Color :</h3>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </div>
+                  <a href="#">View Now</a>
+                </div>
+
+              </div>
+            </div>
+
+            <div class="container">
+              <div class="card">
+                <div class="imgBx">
+                  <img src="<?php echo _WEB_ROOT; ?>/public/assets/client/images/gaming-logo.png" alt="nike-air-shoe">
+                </div>
+
+                <div class="contentBx">
+
+                  <h2>Others</h2>
+
+                  <!-- <div class="size">
+                      <h3>Size :</h3>
+                      <span>7</span>
+                      <span>8</span>
+                      <span>9</span>
+                      <span>10</span>
+                    </div>
+
+                    <div class="color">
+
+                      <h3>Color :</h3>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                    </div> -->
+                  <a href="#">View Now</a>
+                </div>
+
+              </div>
+            </div>
+
+
+
           </div>
         </div>
       </div>
+
     </div>
   </div>
+</div>
