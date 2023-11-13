2bbefe7f904b0a9f8ad4ddef4aaa9351cc05ec45
Nguyen-Quoc-Huy-div <huynqpc05332@fpt.edu.vn>
2023-11-13 22:11:29 +0700

    giao diện user

--- app/views/admin/user/list.php
+++ app/views/admin/user/list.php
@@ -6,79 +6,79 @@
         <table class="table">
           <thead>
             <tr>
-              <th>Profile</th>
-              <th>VatNo.</th>
-              <th>Created</th>
-              <th>Status</th>
-              <th>Action</th>
+              <th class="text-white">Profile</th>
+              <th class="text-white">VatNo.</th>
+              <th class="text-white">Created</th>
+              <th class="text-white">Status</th>
+              <th class="text-white">Action</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>Jacob</td>
-              <td>53275531</td>
-              <td>12 May 2017</td>
-              <td>Activity</td>
+              <td class="text-white">Jacob</td>
+              <td class="text-white">53275531</td>
+              <td class="text-white">12 May 2017</td>
+              <td class="text-white">Activity</td>
+              <td >
+                <a class="badge badge-primary" href="<? echo _MANAGE_DEFAULT ?>/user/user_edit">Edit</a>
+                <a class="badge badge-danger" href="" onclick="return confirm('Bạn Có Chắc Chắn Muốn Xóa'); ">Delete</a>
+              </td>
+            </tr>
+            <tr>
+              <td class="text-white">Jacob</td>  
+              <td class="text-white">53275531</td>
+              <td class="text-white">12 May 2017</td>
+              <td class="text-white">Activity</td>
               <td>
                 <a class="badge badge-primary" href="<? echo _MANAGE_DEFAULT ?>/user/user_edit">Edit</a>
                 <a class="badge badge-danger" href="" onclick="return confirm('Bạn Có Chắc Chắn Muốn Xóa'); ">Delete</a>
               </td>
             </tr>
             <tr>
-              <td>Jacob</td>  
-              <td>53275531</td>
-              <td>12 May 2017</td>
-              <td>Activity</td>
+              <td class="text-white">Jacob</td>
+              <td class="text-white">53275531</td>
+              <td class="text-white">12 May 2017</td>
+              <td class="text-white">Activity</td>
               <td>
                 <a class="badge badge-primary" href="<? echo _MANAGE_DEFAULT ?>/user/user_edit">Edit</a>
                 <a class="badge badge-danger" href="" onclick="return confirm('Bạn Có Chắc Chắn Muốn Xóa'); ">Delete</a>
               </td>
             </tr>
             <tr>
-              <td>Jacob</td>
-              <td>53275531</td>
-              <td>12 May 2017</td>
-              <td>Activity</td>
-              <td>
-                <a class="badge badge-primary" href="<? echo _MANAGE_DEFAULT ?>/user/user_edit">Edit</a>
-                <a class="badge badge-danger" href="" onclick="return confirm('Bạn Có Chắc Chắn Muốn Xóa'); ">Delete</a>
-              </td>
-            </tr>
-            <tr>
-              <td>Messsy</td>
-              <td>53275532</td>
-              <td>12 May 2017</td>
-              <td>Inactivity</td>
+              <td class="text-white">Messsy</td>
+              <td class="text-white">53275532</td>
+              <td class="text-white">12 May 2017</td>
+              <td class="text-white">Inactivity</td>
               <td>
                 <a class="badge badge-primary" href="<? echo _MANAGE_DEFAULT ?>/user/user_edit">Edit</a>
                 <a class="badge badge-danger" href="">Delete</a>
               </td>
             </tr>
             <tr>
-              <td>John</td>
-              <td>53275533</td>
-              <td>12 May 2017</td>
-              <td>Activity</td>
+              <td class="text-white">John</td>
+              <td class="text-white">53275533</td>
+              <td class="text-white">12 May 2017</td>
+              <td class="text-white">Activity</td>
               <td>
                 <a class="badge badge-primary" href="<? echo _MANAGE_DEFAULT ?>/user/user_edit">Edit</a>
                 <a class="badge badge-danger" href="">Delete</a>
               </td>
             </tr>
             <tr>
-              <td>Peter</td>
-              <td>53275534</td>
-              <td>12 May 2017</td>
-              <td>Inactivity</td>
+              <td class="text-white">Peter</td>
+              <td class="text-white">53275534</td>
+              <td class="text-white">12 May 2017</td>
+              <td class="text-white">Inactivity</td>
               <td>
                 <a class="badge badge-primary" href="<? echo _MANAGE_DEFAULT ?>/user/user_edit">Edit</a>
                 <a class="badge badge-danger" href="">Delete</a>
               </td>
             </tr>
             <tr>
-              <td>Dave</td>
-              <td>53275535</td>
-              <td>12 May 2017</td>
-              <td>Activity</td>
+              <td class="text-white">Dave</td>
+              <td class="text-white">53275535</td>
+              <td class="text-white">12 May 2017</td>
+              <td class="text-white">Activity</td>
               <td>
                 <a class="badge badge-primary" href="<? echo _MANAGE_DEFAULT ?>/user/user_edit">Edit</a>
                 <a class="badge badge-danger" href="">Delete</a>
