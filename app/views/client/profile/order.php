01dca4782f61b4cf0c7c95fd584614cc861a7402
dinhkhiem <khiemddpc05680@fpt.edu.vn>
2023-11-14 16:09:42 +0700

    GG - Profile , Order

--- app/views/client/profile/order.php
+++ app/views/client/profile/order.php
@@ -1,89 +1,100 @@
-<section class="rounded" style="background-color:#212529;">
+<section class="rounded" style="background-color:dark;">
     <div class="container py-5">
         <div class="row">
             <div class="col">
-                <nav aria-label="breadcrumb" class="bg-light rounded-3 p-3 mb-4">
-                    <ol class="breadcrumb mb-0">
-                        <li class="breadcrumb-item"><a href="home">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Profile</li>
+                <nav aria-label="breadcrumb" class="bg-dark rounded-3 p-3 mb-4">
+                    <ol class="breadcrumb mb-0 bg-dark">
+                        <li class="breadcrumb-item text-light"><a href="home">Home</a></li>
+                        <li class="breadcrumb-item text-light active" aria-current="page">Profile</li>
                     </ol>
                 </nav>
             </div>
         </div>
 
-        <div class="row">
+        <div class="row ">
             <div class="col-lg-4">
                 <div class="card mb-4">
-                    <div class="card-body text-center">
+                    <div class="card-body text-center bg-dark border-dark">
                         <img src="https://i.pinimg.com/474x/19/05/b5/1905b5aa12fceb753fb75cdc0cd21764.jpg" alt="avatar"
                              class="rounded-circle img-fluid" style="width: 150px;">
                     </div>
                 </div>
-                <div class="card mb-4 mb-lg-0">
-                    <div class="card-body p-0">
+                <div class="card mb-4 mb-lg-0 bg-dark">
+                    <div class="card-body p-0 bg-dark ">
                         <ul class="list-group list-group-flush rounded-3">
-                            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                                <i class="fa fa-user fa-lg text-danger" style="color: #333333;"></i>
-                                <a href="profile" class="mb-0">Thông tin tài khoản</a>
+                            <li class="list-group-item d-flex justify-content-between align-items-center p-3 bg-dark">
+                                <i class="fa fa-user fa-lg text-light"></i>
+                                <a href="profile" class="mb-0 text-light">Thông tin tài khoản</a>
                             </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                                <i class="fa fa-shopping-basket fa-lg text-warning"></i>
+                            <li class="list-group-item d-flex justify-content-between align-items-center p-3 bg-dark">
+                                <i class="fa fa-shopping-basket fa-lg text-danger "></i>
                                 <a href="" class="mb-0 text-danger">Quản lý đơn hàng</a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-8">
-                <div class="card mb-5">
+            <div class="col-lg-8  ">
+                <div class="card mb-5 bg-dark">
                     <div class="card-body">
                         <div class="row">
-                            <form action="#" method="POST">
-                                <h4 class="text-dark">Thông tin tài khoản</h4><br>
-                                <div class="form-row pl-5">
-                                    <div class="form-group col-8 d-flex align-items-center">
-                                        <label for="full_name" class="col-form-label mr-5">Họ và tên </label>
-                                        <input type="text" class="form-control col-6" id="full_name" name="full_name"
-                                               required>
+                            <h3 class="text-light">Quản lý đơn hàng</h3>
+                            <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+                                <div class="container-fluid">
+                                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 bg-dark text-light">
+                                            <li class="nav-item">
+                                                <a class="nav-link active  text-light" aria-current="page" href="#">Tất
+                                                    cả</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link  text-light" href="#">Mới </a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link  text-light" href="#">Đang xử lí</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link  text-light" href="#">Đang vận chuyển</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link  text-light" href="#">Hoàn thành</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link  text-light" href="#">Hủy</a>
+                                            </li>
+                                        </ul>
                                     </div>
-
-                                    <div class="form-group col-8 d-flex align-items-center">
-                                        <label for="gender" class="col-form-label mr-5">Giới tính</label>
-                                        <div class="custom-control custom-radio custom-control-inline  ml-2">
-                                            <input type="radio" id="male" name="gender" class="custom-control-input"
-                                                   value="male" required>
-                                            <label class="custom-control-label" for="male">Nam</label>
-                                        </div>
-
-                                        <div class="custom-control custom-radio custom-control-inline">
-                                            <input type="radio" id="female" name="gender" class="custom-control-input"
-                                                   value="female" required>
-                                            <label class="custom-control-label" for="female">Nữ</label>
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group col-8 d-flex align-items-center">
-                                        <label for="phone_number" class="col-form-label mr-4">Số điện thoại </label>
-                                        <label for="phone_number" class="col-form-label mr-3">*********** </label>
-                                    </div>
-
-                                    <div class="form-group col-8 d-flex align-items-center">
-                                        <label for="email" class="col-form-label mr-5 ">Email :</label>
-                                        <label for="email" class="col-form-label ml-4"> gaminggear@fpt.edu.vn</label>
-                                    </div>
-
-                                    <div class="form-group col-8 d-flex align-items-center">
-                                        <label for="birthday" class="col-form-label mr-3 ">Ngày sinh </label>
-                                        <input type="date" class="form-control ml-5" id="birthday" name="birthday"
-                                               required>
-                                    </div>
-                            </form>
+                                </div>
+                            </nav>
                         </div>
-                        <button type="submit" class="btn btn-danger mx-auto" style="width: 100px;">Cập nhật</button>
+                    </div>
+                </div>
+                <!-- <div class="card mb-5 d-flex">
+                  <div class="card-body">
+                    <div class="row d-flex justify-content-between">
+                      <div class="search d-flex">
+                        <i class="fa fa-search"></i>
+                        <a>Tìm đơn hàng theo Mã đơn hàng</a>
+                        <span class="line"></span>
+                      </div>
+                      <div class="search-button">
+                        <button type="submit" class="text-primary border-0 bg-light">Tìm đơn hàng</button>
+                      </div>
+                    </div>
+                  </div>
+                </div> -->
+                <div class="card d-flex flex-column text-center">
+                    <div class="card-body bg-dark">
+                        <div class="row d-flex justify-content-center  ">
+                            <div class="message">
+                                <i class="fa fa-store-slash fa-2xl text-light"></i><br> <br>
+                                <p class="text-light">Quý khách chưa có đơn hàng nào.</p><br>
+                                <button class="bg-danger text-white border-danger rounded">Tiếp tục mua hàng</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
     </div>
     </div>
 </section>
--- app/views/client/profile/profile.php
+++ app/views/client/profile/profile.php
@@ -1,89 +1,87 @@
-<section class="rounded" style="background-color:#212529;">
+<section class="rounded" style="background-color:dark;">
     <div class="container py-5">
         <div class="row">
             <div class="col">
-                <nav aria-label="breadcrumb" class="bg-light rounded-3 p-3 mb-4">
-                    <ol class="breadcrumb mb-0">
-                        <li class="breadcrumb-item"><a href="home">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Profile</li>
+                <nav aria-label="breadcrumb" class="bg-dark rounded-3 p-3 mb-4">
+                    <ol class="breadcrumb mb-0 bg-dark">
+                        <li class="breadcrumb-item text-light"><a href="home">Home</a></li>
+                        <li class="breadcrumb-item text-light active" aria-current="page">Profile</li>
                     </ol>
                 </nav>
             </div>
         </div>
 
-        <div class="row">
+        <div class="row ">
             <div class="col-lg-4">
                 <div class="card mb-4">
-                    <div class="card-body text-center">
+                    <div class="card-body text-center bg-dark border-dark">
                         <img src="https://i.pinimg.com/474x/19/05/b5/1905b5aa12fceb753fb75cdc0cd21764.jpg" alt="avatar"
                              class="rounded-circle img-fluid" style="width: 150px;">
                     </div>
                 </div>
-                <div class="card mb-4 mb-lg-0">
-                    <div class="card-body p-0">
+                <div class="card mb-4 mb-lg-0 bg-light">
+                    <div class="card-body p-0 bg-dark ">
                         <ul class="list-group list-group-flush rounded-3">
-                            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                                <i class="fa fa-user fa-lg text-danger" style="color: #333333;"></i>
+                            <li class="list-group-item d-flex justify-content-between align-items-center p-3 bg-dark">
+                                <i class="fa fa-user fa-lg text-danger"></i>
                                 <a href="" class="mb-0 text-danger">Thông tin tài khoản</a>
                             </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                                <i class="fa fa-shopping-basket fa-lg text-warning"></i>
-                                <a href="order" class="mb-0">Quản lý đơn hàng</a>
+                            <li class="list-group-item d-flex justify-content-between align-items-center p-3 bg-dark">
+                                <i class="fa fa-shopping-basket fa-lg text-light "></i>
+                                <a href="order" class="mb-0 text-light">Quản lý đơn hàng</a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-8">
-                <div class="card mb-5">
-                    <div class="card-body">
-                        <div class="row">
-                            <form action="#" method="POST">
-                                <h4 class="text-dark">Thông tin tài khoản</h4><br>
-                                <div class="form-row pl-5">
-                                    <div class="form-group col-8 d-flex align-items-center">
-                                        <label for="full_name" class="col-form-label mr-5">Họ và tên </label>
-                                        <input type="text" class="form-control col-6" id="full_name" name="full_name"
-                                               required>
+            <div class="col-lg-8 bg-dark rounded ">
+                <div class="card-body">
+                    <div class="row bg-dark text-light">
+                        <form action="#" method="POST">
+                            <h4 class="text-light">Thông tin tài khoản</h4><br>
+                            <div class="form-row pl-5">
+                                <div class="form-group col-8 d-flex align-items-center">
+                                    <label for="full_name" class="col-form-label mr-5">Họ và tên </label>
+                                    <input type="text" class="form-control col-6" id="full_name" name="full_name"
+                                           required>
+                                </div>
+
+                                <div class="form-group col-8 d-flex align-items-center">
+                                    <label for="gender" class="col-form-label mr-5">Giới tính</label>
+                                    <div class="custom-control custom-radio custom-control-inline  ml-2">
+                                        <input type="radio" id="male" name="gender" class="custom-control-input"
+                                               value="male" required>
+                                        <label class="custom-control-label" for="male">Nam</label>
                                     </div>
 
-                                    <div class="form-group col-8 d-flex align-items-center">
-                                        <label for="gender" class="col-form-label mr-5">Giới tính</label>
-                                        <div class="custom-control custom-radio custom-control-inline  ml-2">
-                                            <input type="radio" id="male" name="gender" class="custom-control-input"
-                                                   value="male" required>
-                                            <label class="custom-control-label" for="male">Nam</label>
-                                        </div>
-
-                                        <div class="custom-control custom-radio custom-control-inline">
-                                            <input type="radio" id="female" name="gender" class="custom-control-input"
-                                                   value="female" required>
-                                            <label class="custom-control-label" for="female">Nữ</label>
-                                        </div>
+                                    <div class="custom-control custom-radio custom-control-inline">
+                                        <input type="radio" id="female" name="gender" class="custom-control-input"
+                                               value="female" required>
+                                        <label class="custom-control-label" for="female">Nữ</label>
                                     </div>
+                                </div>
 
-                                    <div class="form-group col-8 d-flex align-items-center">
-                                        <label for="phone_number" class="col-form-label mr-4">Số điện thoại </label>
-                                        <label for="phone_number" class="col-form-label mr-3">*********** </label>
-                                    </div>
+                                <div class="form-group col-8 d-flex align-items-center">
+                                    <label for="phone_number" class="col-form-label mr-4">Số điện thoại </label>
+                                    <label for="phone_number" class="col-form-label mr-3">*********** </label>
+                                </div>
 
-                                    <div class="form-group col-8 d-flex align-items-center">
-                                        <label for="email" class="col-form-label mr-5 ">Email :</label>
-                                        <label for="email" class="col-form-label ml-4"> gaminggear@fpt.edu.vn</label>
-                                    </div>
+                                <div class="form-group col-8 d-flex align-items-center">
+                                    <label for="email" class="col-form-label mr-5 ">Email :</label>
+                                    <label for="email" class="col-form-label ml-4"> gaminggear@fpt.edu.vn</label>
+                                </div>
 
-                                    <div class="form-group col-8 d-flex align-items-center">
-                                        <label for="birthday" class="col-form-label mr-3 ">Ngày sinh </label>
-                                        <input type="date" class="form-control ml-5" id="birthday" name="birthday"
-                                               required>
-                                    </div>
-                            </form>
-                        </div>
-                        <button type="submit" class="btn btn-danger mx-auto" style="width: 100px;">Cập nhật</button>
+                                <div class="form-group col-8 d-flex align-items-center">
+                                    <label for="birthday" class="col-form-label mr-3 ">Ngày sinh </label>
+                                    <input type="date" class="form-control ml-5" id="birthday" name="birthday" required>
+                                </div>
+                        </form>
                     </div>
+                    <button type="submit" class="btn btn-danger mx-auto" style="width: 100px;">Cập nhật</button>
                 </div>
             </div>
         </div>
+    </div>
     </div>
     </div>
 </section>
