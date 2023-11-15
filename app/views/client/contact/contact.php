1bc70335bcade1967eed7f92664a475a211703ea
phuong05429 <phuongncpc05429@fpt.edu.vn>
2023-11-15 14:49:21 +0700

    GG-13-contact

---
+++ app/views/client/contact/contact.php
@@ -0,0 +1,137 @@
+<div class="row">
+    <div class="col-lg-12">
+       
+            <!-- ***** Featured Start ***** -->
+            <div class="row-ct">
+
+                <div class="col-lg-12">
+                    <div class="feature-banner header-text">
+                        <h1 class="mb-4"> Contact with <em>Us</em></h1>
+                        <iframe class="rounded-4" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44453.48366464558!2d105.77759912435515!3d10.002815225715315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a08906415c355f%3A0x416815a99ebd841e!2sFPT%20Polytechnic%20College!5e0!3m2!1sen!2s!4v1699266001620!5m2!1sen!2s" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+
+                    <div class="background">
+                        <div class="container">
+                            <div class="screen">
+                                
+                                <div class="screen-body">
+                                    <div class="screen-body-item left">
+                                        <div class="app-title">
+                                            <span>CONTACT</span>
+                                            <span>FROM</span>
+                                        </div>
+                                       
+                                        <div class="app-contact"></div>
+                                    </div>
+                                    <div class="screen-body-item">
+                                        <div class="app-form">
+                                            <div class="app-form-group">
+                                                <input class="app-form-control" placeholder="NAME">
+                                            </div>
+                                            <div class="app-form-group">
+                                                <input class="app-form-control" placeholder="EMAIL">
+                                            </div>
+                                            <div class="app-form-group">
+                                                <input class="app-form-control" placeholder="CONTACT NO">
+                                            </div>
+                                            <div class="app-form-group message">
+                                                <input class="app-form-control" placeholder="MESSAGE">
+                                            </div>
+                                            <div class="app-form-group buttons">
+                                                <button class="app-form-button">CANCEL</button>
+                                                <button class="app-form-button">SEND</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                </div>
+
+
+
+            </div>
+            <!-- ***** Featured End ***** -->
+
+            <!-- ***** Details Start ***** -->
+            <div class="game-details">
+                <div class="row">
+
+
+                </div>
+            </div>
+            <!-- ***** Details End ***** -->
+
+            <!-- ***** Other Start ***** -->
+            <div class="other-games">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="heading-section">
+                            <h4>Category</h4>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="item d-flex align-items-center justify-content-between p-3">
+                            <div class="">
+                                <img src="assets/images/game-01.jpg" alt="" class="templatemo-item">
+                                <h4>Name Category</h4>
+                            </div>
+                            <button style="background-color: #ec6090; color: #fff; padding: 10px;" class="border-0 rounded-5">View</button>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="item d-flex align-items-center justify-content-between p-3">
+                            <div class="">
+                                <img src="assets/images/game-01.jpg" alt="" class="templatemo-item">
+                                <h4>Name Category</h4>
+                            </div>
+                            <button style="background-color: #ec6090; color: #fff; padding: 10px;" class="border-0 rounded-5">View</button>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="item d-flex align-items-center justify-content-between p-3">
+                            <div class="">
+                                <img src="assets/images/game-01.jpg" alt="" class="templatemo-item">
+                                <h4>Name Category</h4>
+                            </div>
+                            <button style="background-color: #ec6090; color: #fff; padding: 10px;" class="border-0 rounded-5">View</button>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="item d-flex align-items-center justify-content-between p-3">
+                            <div class="">
+                                <img src="assets/images/game-01.jpg" alt="" class="templatemo-item">
+                                <h4>Name Category</h4>
+                            </div>
+                            <button style="background-color: #ec6090; color: #fff; padding: 10px;" class="border-0 rounded-5">View</button>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="item d-flex align-items-center justify-content-between p-3">
+                            <div class="">
+                                <img src="assets/images/game-01.jpg" alt="" class="templatemo-item">
+                                <h4>Name Category</h4>
+                            </div>
+                            <button style="background-color: #ec6090; color: #fff; padding: 10px;" class="border-0 rounded-5">View</button>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="item d-flex align-items-center justify-content-between p-3">
+                            <div class="">
+                                <img src="assets/images/game-01.jpg" alt="" class="templatemo-item">
+                                <h4>Name Category</h4>
+                            </div>
+                            <button style="background-color: #ec6090; color: #fff; padding: 10px;" class="border-0 rounded-5">View</button>
+                        </div>
+                    </div>
+                </div>
+                <!-- ***** Other End ***** -->
+
+            </div>
+      
+    </div>
+</div>
