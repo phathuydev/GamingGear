03d41ce47dbe4d272e451ab4c9170eb1f4ac1137
Lữ Phát Huy <huylppc05334@fpt.edu.vn>
2023-11-15 18:08:12 +0700

    update contact

--- configs/routes.php
+++ configs/routes.php
@@ -2,7 +2,7 @@
 $routes['default_controller'] = 'home';
 // Đường dẫn ảo => đường dẫn thật
 // Admin
-$routes['gg-login'] = 'admin/login'; 
+$routes['lgAdmin'] = 'admin/login/get_login';
 $routes['gg-admin'] = 'admin/dashboard';
 // Client
 $routes['home'] = 'home';
@@ -11,7 +11,18 @@ $routes['product-detail'] = 'client/product/product_detail';
 $routes['product-search'] = 'client/product/product_search';
 $routes['product-category'] = 'client/product/product_category';
 $routes['cart'] = 'client/cart/cart';
+$routes['profile'] = 'client/profile/profile';
+$routes['orderHistory'] = 'client/profile/order';
+$routes['post'] = 'client/post/post';
 $routes['contact'] = 'client/contact/contact';
+$routes['postDetail'] = 'client/post/detail';
 $routes['checkout'] = 'client/cart/checkout';
 $routes['thank'] = 'client/cart/thank';
+$routes['lgUser'] = 'client/account/login';
+$routes['profile'] = 'client/profile/profile';
+$routes['order'] = 'client/profile/order';
+$routes['logout'] = 'client/account/logout';
+$routes['register'] = 'client/account/register';
+$routes['forgot'] = 'client/account/forgot';
+$routes['resetpass'] = 'client/account/resetpass';
 $routes['post/.+-(\d+).php'] = 'client/post/post/category/$1';
