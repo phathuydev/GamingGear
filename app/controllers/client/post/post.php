b7b5ab4a73a9cd65ec28e2db679b7955975aff78
Lữ Phát Huy <huylppc05334@fpt.edu.vn>
2023-11-15 18:13:36 +0700

    Update css post

--- app/controllers/client/post/post.php
+++ app/controllers/client/post/post.php
@@ -3,16 +3,19 @@ class Post extends Controller
 {
     public $province;
     public $data = [];
+
     public function __construct()
     {
-      $this->province = $this->model('');
+        $this->province = $this->model('');
     }
+
     public function index()
     {
-      $title = 'Post List';
-      $this->data['pages_title'] = $title;
-      $this->data['sub_content']['post'] = [];
-      $this->data['content'] = 'client/post/list';
-      $this->render('client/layoutClient/client_layout', $this->data);
+        $title = 'Post List';
+        $this->data['pages_title'] = $title;
+        $this->data['css_post'] = _WEB_ROOT . '/public/assets/client/css/post.css';
+        $this->data['sub_content']['post'] = [];
+        $this->data['content'] = 'client/post/list';
+        $this->render('client/layoutClient/client_layout', $this->data);
     }
 }
