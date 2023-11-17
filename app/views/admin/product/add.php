dda8fd22c5dc9bcd1daf5ec65e2c6ec2c5d25f9a
Nguyen-Quoc-Huy-div <huynqpc05332@fpt.edu.vn>
2023-11-17 19:33:02 +0700

    giao diện prduct admin

--- app/views/admin/product/add.php
+++ app/views/admin/product/add.php
@@ -1,1 +1,39 @@
-<h1>Add Product Pages</h1>
+<div class="col-md-12 grid-margin stretch-card">
+  <div class="card">
+    <div class="card-body">
+      <h4 class="card-title">Add Add</h4>
+      <form class="forms-sample">
+        <div class="form-group">
+          <label for="exampleInputUsername1">Name Product</label>
+          <input type="text" class="form-control" id="" placeholder="name product">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Image</label>
+          <input type="file" class="form-control" id="" placeholder="image">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">price</label>
+          <input type="num" class="form-control" id="" placeholder="price">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputConfirmPassword1">Sale</label>
+          <input type="num" class="form-control" id="" placeholder="sale">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputConfirmPassword1">category</label>
+          <input type="text" class="form-control" id="" placeholder="category">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputConfirmPassword1">Speciel</label>
+          <input type="text" class="form-control" id="" placeholder="speciel">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputConfirmPassword1">Describe</label>
+          <input type="text" class="form-control" id="" placeholder="desctibe">
+        </div>
+        <button type="submit" class="btn btn-primary mr-2">Submit</button>
+        <a class="btn btn-dark" href="<? echo _MANAGE_DEFAULT ?>/user">Cancel</a>
+      </form>
+    </div>
+  </div>
+</div>  
--- app/views/admin/product/list.php
+++ app/views/admin/product/list.php
@@ -1,1 +1,146 @@
-<h1>Product Pages</h1>
+<div class="col-lg-12 grid-margin stretch-card">
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">List Products</h4>
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th class="text-white">ID</th>
+                            <th class="text-white">Name</th>
+                            <th class="text-white">Image</th>
+                            <th class="text-white">Price</th>
+                            <th class="text-white">Sale</th>
+                            <th class="text-white">Quantity</th>
+                            <th class="text-white">Category</th>
+                            <th class="text-white">View</th>
+                            <th class="text-white">Special</th>
+                            <th class="text-white pl-4">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="text-white">1</td>
+                            <td class="text-white">MOUSE CORSAIR M65</td>
+                            <td class=""><img src="<? echo _WEB_ROOT ?>/public/assets/client/images/product-1.png" alt=""
+                                    style="width: 70px !important; height: 70px" ;></td>
+                            <td class="text-white">79$</td>
+                            <td class="text-white">61$</td>
+                            <td class="text-white">23</td>
+                            <td class="text-white">Mouse</td>
+                            <td class="text-white">564</td>
+                            <td class="text-white">New</td>
+                            <td>
+                                <a class="badge badge-primary" href="<? echo _MANAGE_DEFAULT ?>/product/product_edit">Edit</a>
+                                <a class="badge badge-danger" href=""
+                                    onclick="return confirm('Bạn Có Chắc Chắn Muốn Xóa'); ">Delete</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="text-white">1</td>
+                            <td class="text-white">MOUSE CORSAIR M65</td>
+                            <td class=""><img src="<? echo _WEB_ROOT ?>/public/assets/client/images/product-1.png" alt=""
+                                    style="width: 70px !important; height: 70px" ;></td>
+                            <td class="text-white">79$</td>
+                            <td class="text-white">61$</td>
+                            <td class="text-white">23</td>
+                            <td class="text-white">Mouse</td>
+                            <td class="text-white">564</td>
+                            <td class="text-white">New</td>
+                            <td>
+                                <a class="badge badge-primary" href="<? echo _MANAGE_DEFAULT ?>/user/user_edit">Edit</a>
+                                <a class="badge badge-danger" href=""
+                                    onclick="return confirm('Bạn Có Chắc Chắn Muốn Xóa'); ">Delete</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="text-white">1</td>
+                            <td class="text-white">MOUSE CORSAIR M65</td>
+                            <td class=""><img src="<? echo _WEB_ROOT ?>/public/assets/client/images/product-1.png" alt=""
+                                    style="width: 70px !important; height: 70px" ;></td>
+                            <td class="text-white">79$</td>
+                            <td class="text-white">61$</td>
+                            <td class="text-white">23</td>
+                            <td class="text-white">Mouse</td>
+                            <td class="text-white">564</td>
+                            <td class="text-white">New</td>
+                            <td>
+                                <a class="badge badge-primary" href="<? echo _MANAGE_DEFAULT ?>/user/user_edit">Edit</a>
+                                <a class="badge badge-danger" href=""
+                                    onclick="return confirm('Bạn Có Chắc Chắn Muốn Xóa'); ">Delete</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="text-white">1</td>
+                            <td class="text-white">MOUSE CORSAIR M65</td>
+                            <td class=""><img src="<? echo _WEB_ROOT ?>/public/assets/client/images/product-1.png" alt=""
+                                    style="width: 70px !important; height: 70px" ;></td>
+                            <td class="text-white">79$</td>
+                            <td class="text-white">61$</td>
+                            <td class="text-white">23</td>
+                            <td class="text-white">Mouse</td>
+                            <td class="text-white">564</td>
+                            <td class="text-white">New</td>
+                            <td>
+                                <a class="badge badge-primary" href="<? echo _MANAGE_DEFAULT ?>/user/user_edit">Edit</a>
+                                <a class="badge badge-danger" href=""
+                                    onclick="return confirm('Bạn Có Chắc Chắn Muốn Xóa'); ">Delete</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="text-white">1</td>
+                            <td class="text-white">MOUSE CORSAIR M65</td>
+                            <td class=""><img src="<? echo _WEB_ROOT ?>/public/assets/client/images/product-1.png" alt=""
+                                    style="width: 70px !important; height: 70px" ;></td>
+                            <td class="text-white">79$</td>
+                            <td class="text-white">61$</td>
+                            <td class="text-white">23</td>
+                            <td class="text-white">Mouse</td>
+                            <td class="text-white">564</td>
+                            <td class="text-white">New</td>
+                            <td>
+                                <a class="badge badge-primary" href="<? echo _MANAGE_DEFAULT ?>/user/user_edit">Edit</a>
+                                <a class="badge badge-danger" href=""
+                                    onclick="return confirm('Bạn Có Chắc Chắn Muốn Xóa'); ">Delete</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="text-white">1</td>
+                            <td class="text-white">MOUSE CORSAIR M65</td>
+                            <td class=""><img src="<? echo _WEB_ROOT ?>/public/assets/client/images/product-1.png" alt=""
+                                    style="width: 70px !important; height: 70px" ;></td>
+                            <td class="text-white">79$</td>
+                            <td class="text-white">61$</td>
+                            <td class="text-white">23</td>
+                            <td class="text-white">Mouse</td>
+                            <td class="text-white">564</td>
+                            <td class="text-white">New</td>
+                            <td>
+                                <a class="badge badge-primary" href="<? echo _MANAGE_DEFAULT ?>/user/user_edit">Edit</a>
+                                <a class="badge badge-danger" href=""
+                                    onclick="return confirm('Bạn Có Chắc Chắn Muốn Xóa'); ">Delete</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="text-white">1</td>
+                            <td class="text-white">MOUSE CORSAIR M65</td>
+                            <td class=""><img src="<? echo _WEB_ROOT ?>/public/assets/client/images/product-1.png" alt=""
+                                    style="width: 70px !important; height: 70px" ;></td>
+                            <td class="text-white">79$</td>
+                            <td class="text-white">61$</td>
+                            <td class="text-white">23</td>
+                            <td class="text-white">Mouse</td>
+                            <td class="text-white">564</td>
+                            <td class="text-white">New</td>
+                            <td>
+                                <a class="badge badge-primary" href="<? echo _MANAGE_DEFAULT ?>/user/user_edit">Edit</a>
+                                <a class="badge badge-danger" href=""
+                                    onclick="return confirm('Bạn Có Chắc Chắn Muốn Xóa'); ">Delete</a>
+                            </td>
+                        </tr>   
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
