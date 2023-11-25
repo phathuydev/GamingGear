936f646e09a46f851c008e6932efd407c2b3a422
phuong05429 <phuongncpc05429@fpt.edu.vn>
2023-11-25 21:02:14 +0700

    GG-thanks(2)

--- app/views/client/cart/thanks.php
+++ app/views/client/cart/thanks.php
@@ -1,6 +1,6 @@
 <div class="content-thanks">
-    <h2 class="text-shadows-thanks">Thanks You !</h2><br>
-    <h4>GammingGear</h4>
+    <h2 class="text-shadows-thanks">Thank You !</h2><br>
+    <h4>GammingGear.vn</h4>
     <p class='content-thk'>Thank you customers for trusting our products and choosing to purchase at our store. Your
         order has been received and will contact you later, <i>thank you very much!</i></p>
 </div>
