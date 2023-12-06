77b98e00bb8ce80cf680b393f9c5bb765342a738
Lữ Phát Huy <huylppc05334@fpt.edu.vn>
2023-12-06 11:50:32 +0700

    Sửa code trạng thái order admin

--- app/views/admin/order/list.php
+++ app/views/admin/order/list.php
@@ -12,6 +12,7 @@
               <th class="text-white">Phone</th>
               <th class="text-white">Address</th>
               <th class="text-white">Total</th>
+              <th class="text-white">Status</th>
               <th class="text-white">Action</th>
             </tr>
           </thead>
@@ -24,10 +25,14 @@
                 <td class="text-white"><?= $item['user_phone']; ?></td>
                 <td class="text-white"><?= $item['user_address']; ?></td>
                 <td class="text-white"><?= '$' . $item['order_total']; ?></td>
-                <td class="d-flex align-items-center">
-                  <a class="badge badge-primary mr-2" href="<?php echo _MANAGE_DEFAULT ?>/order/order_detail/<?= $item['order_id']; ?>">See detail</a>
-                  <a class="badge badge-success mr-2" href="<?php echo _MANAGE_DEFAULT ?>/order/order_edit/<?= $item['order_id']; ?>/<?= $item['order_status']; ?>">Update status</a>
-                </td>
+                <?php $checkFailOrder = $this->model('OrderModel')->countOrderDetail($item['order_id']); ?>
+                <?php foreach ($checkFailOrder as $data) : ?>
+                  <td class="text-white"><?= $data == 0 ? '<div class="badge badge-outline-danger">Cancelled</div>' : '<div class="badge badge-outline-success">Not cancelled</div>' ?></td>
+                  <td class="d-flex align-items-center">
+                    <a class="badge badge-primary mr-2 <?= $data == 0 ? 'disabled' : false ?>" href="<?php echo _MANAGE_DEFAULT ?>/order/order_detail/<?= $item['order_id']; ?>">See detail</a>
+                    <a class="badge badge-success mr-2 <?= $data == 0 ? 'disabled' : false ?>" href="<?php echo _MANAGE_DEFAULT ?>/order/order_edit/<?= $item['order_id']; ?>/<?= $item['order_status']; ?>">Update status</a>
+                  </td>
+                <?php endforeach ?>
               </tr>
             <?php endforeach; ?>
           </tbody>
